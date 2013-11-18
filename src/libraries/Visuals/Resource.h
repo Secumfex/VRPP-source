9a97c348e6b10855d2bbc5b2af7dbe260aac4516
raphimulator <rheidrich@uni-koblenz.de>
2013-11-18 18:46:49 +0100

    h-Datei
    ganz tolle Variablen und so

--- src/libraries/Visuals/Resource.h
+++ src/libraries/Visuals/Resource.h
@@ -1,16 +1,42 @@
 #ifndef RESOURCE_H_
 #define RESOURCE_H_
 
+#include <vector>
+
+using namespace std;
+
 class Resource{
 public:
 	virtual void streamToVRAM();
+	virtual void getResource(const char* filename);
+
 };
 
 class Texture : public Resource {
 
+	//---------------MEMBER FUNCTIONS--------------------
+public:
+	void getResource(const char* filename);
+
+	//---------------MEMBER VARIABLES--------------------
 };
 
 class Mesh : public Resource{
+
+	//---------------MEMBER FUNCTIONS--------------------
+public:
+	void getResource(const char* filename);
+	vector<float> getVertices();
+	vector<float> getIndices();
+	vector<float> getNormals();
+	vector<float> getUVs();
+
+	//---------------MEMBER VARIABLES--------------------
+private:
+	vector<float> mVertices;
+	vector<unsigned int> mIndices;
+	vector<float> mNormals;
+	vector<float> mUVs;
 
 };
 
