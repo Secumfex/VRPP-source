d551b41a6b389d51047f17229e1e42adedc5c65a
Ira120 <irinaskl@uni-koblenz.de>
2013-11-19 10:29:43 +0100

    Laden des Objekts funktioniert...
    Conflicts:
    src/libraries/Visuals/Resource.h

--- src/libraries/Visuals/Resource.cpp
+++ src/libraries/Visuals/Resource.cpp
@@ -1,6 +1,24 @@
 #include "Resource.h"
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <GLFW/glfw3.h>
 
-//---------------MESH SCOPE--------------------
+
+//---------------RESOURCE SCOPE--------------------
+
+void Resource :: setName(char* name){
+	mName = name;
+
+}
+
+
+//---------------TEXTURE SCOPE--------------------
+
+void Texture :: streamToVRAM(){
+
+}
+
 void Texture :: getResource(const char* filename){
 
 	//TODO: GLMModel machen
@@ -15,8 +33,77 @@ void Texture :: getResource(const char* filename){
 }
 
 //---------------MESH SCOPE--------------------
-void Mesh :: getResource(const char* filename){
 
+void Mesh :: streamToVRAM(){
+
+	glGenBuffers(4, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(mVertices), &mVertices[0], GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+//
+//	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//	glBufferData(GL_ARRAY_BUFFER, sizeof(mIndices), mIndices, GL_STATIC_DRAW);
+//	glVertexAttribPointer(0, 3, GL_INT, GL_FALSE, 0, 0);
+//
+//	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//	glBufferData(GL_ARRAY_BUFFER, sizeof(mNormals), mNormals, GL_STATIC_DRAW);
+//	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+//
+//	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//	glBufferData(GL_ARRAY_BUFFER, sizeof(mUVs), mUVs, GL_STATIC_DRAW);
+//	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+//
+
+}
+
+void Mesh :: getResource(const char* filename){
+    
+    ifstream in(filename, ios::in);
+    if (!in)
+    {
+        cerr << "Objekt kann nicht geoeffnet werden..." << filename << endl;
+        exit(1);
+        
+    }
+    cout << "Objekt geoeffnet..." << filename << endl;
+    std::string line;
+    while (std::getline(in, line))
+    {
+        //cout<<line<<endl;
+        if (line.substr(0,2)=="v "){
+            std::istringstream v(line.substr(2));
+            glm::vec3 vert;
+            double x,y,z;
+            v>>x;v>>y;v>>z;
+            vert=glm::vec3(x,y,z);
+            mVertices.push_back(vert);
+        }
+        else if(line.substr(0,2)=="f "){
+            std::istringstream v(line.substr(2));
+            GLuint a,b,c;
+            v>>a;v>>b;v>>c;
+            a--;b--;c--;
+            mIndices.push_back(a);
+            mIndices.push_back(b);
+            mIndices.push_back(c);
+        }
+        
+    }
+    for(unsigned int i=0;i<mIndices.size();i++)
+    {
+        glm::vec3 meshData;
+        meshData=glm::vec3(mVertices[mIndices[i]].x,mVertices[mIndices[i]].y,mVertices[mIndices[i]].z);
+        meshVertices.push_back(meshData);
+        
+    }
+}
+
+vector<glm::vec3> Mesh::returnMesh()
+{
+    return meshVertices;
+}
+    
 	//TODO: GLMModel machen
 
 	//TODO: file finden und das Model damit füttern
@@ -24,6 +111,6 @@ void Mesh :: getResource(const char* filename){
 	//TODO: Alle notwendigen Daten aus dem Model lesen
 
 	//TODO: Model löschen
-}
+
 
 
--- src/libraries/Visuals/Resource.h
+++ src/libraries/Visuals/Resource.h
@@ -1,15 +1,38 @@
 #ifndef RESOURCE_H_
 #define RESOURCE_H_
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <vector>
 #include <glm/glm.hpp>
+<<<<<<< HEAD
+#include <iostream>
+#include <fstream>
+=======
 
+
+>>>>>>> 60d3bb6c9f47411894c4c4880de7d1065b5225d5
 using namespace std;
 
+
 class Resource{
+
+	//---------------MEMBER FUNCTIONS--------------------
 public:
+<<<<<<< HEAD
+	//virtual void streamToVRAM();
+	//virtual void getResource(const char* filename);
+=======
 	virtual void streamToVRAM();
 	virtual void getResource(const char* filename);
+	virtual void setName(char* name);
+	//---------------MEMBER VARIABLES--------------------
+private:
+	char* mName;
+
+public:
+	GLuint VBO;
+>>>>>>> 60d3bb6c9f47411894c4c4880de7d1065b5225d5
 
 };
 
@@ -17,6 +40,8 @@ class Texture : public Resource {
 
 	//---------------MEMBER FUNCTIONS--------------------
 public:
+	Texture ();
+	void streamToVRAM();
 	void getResource(const char* filename);
 
 	//---------------MEMBER VARIABLES--------------------
@@ -26,7 +51,9 @@ class Mesh : public Resource{
 
 	//---------------MEMBER FUNCTIONS--------------------
 public:
+	void streamToVRAM();
 	void getResource(const char* filename);
+    vector<glm::vec3> returnMesh();//return the vertices of mesh data
 	vector<float> getVertices();
 	vector<float> getIndices();
 	vector<float> getNormals();
@@ -34,10 +61,11 @@ public:
 
 	//---------------MEMBER VARIABLES--------------------
 private:
-	vector<float> mVertices;
-	vector<unsigned int> mIndices;
+	vector<glm::vec3> mVertices;
+	vector<int> mIndices;
 	vector<float> mNormals;
 	vector<float> mUVs;
+    vector<glm::vec3> meshVertices;//to store all 3D model face vertices
 
 };
 
