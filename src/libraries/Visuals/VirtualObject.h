b2f53308238d4afe8477e9d3d994564a51f4730d
traubenzucker3001 <traubenzucker3001@googlemail.com>
2013-11-21 16:20:18 +0100

    VirtualObject begonnen
    erstes Test-Hochladen, nur .h etwas bearbeitet

--- src/libraries/Visuals/VirtualObject.h
+++ src/libraries/Visuals/VirtualObject.h
@@ -1,16 +1,20 @@
-
 #ifndef VIRTUALOBJECT_H
 #define VIRTUALOBJECT_H
 #include "Physics/PhysicsComponent.h"
 #include "Visuals/GraphicsComponent.h"
+#include <glm/glm.hpp>
 
 class VirtualObject{
 public:
 
 	//TODO Model Matrix
+	glm::mat4 modelMatrix;
 
+	int id;
 	PhysicsComponent physicsComponent;
 	GraphicsComponent graphicsComponent;
+
+	VirtualObject();
 
 	void render();
 };
