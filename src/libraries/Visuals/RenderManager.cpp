370e9ffaa7d1104aeb792d6e564c5563f3d2e0d8
RuuNa <qala@uni-koblenz.de>
2013-11-25 16:01:44 +0100

    Renderer WIP: adding includes

--- src/libraries/Visuals/RenderManager.cpp
+++ src/libraries/Visuals/RenderManager.cpp
@@ -1,3 +1,17 @@
 #include "RenderManager.h"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <string.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+
+
+
 void RenderManager :: renderLoop(){std::cout<<"renderLoop()..."<<std::endl; }
