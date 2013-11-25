f496c4827500ad2ceb46fe55cef3ab72284a8f3b
RuuNa <qala@uni-koblenz.de>
2013-11-25 17:20:43 +0100

    Renderer WIP: readding includes
    Issues adding Geometry.h -> multiple definitions error

--- src/libraries/Visuals/RenderManager.cpp
+++ src/libraries/Visuals/RenderManager.cpp
@@ -1,7 +1,23 @@
 #include "RenderManager.h"
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 #include <string.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+
+#include "Tools/ShaderTools.h"
+#include "Tools/TextureTools.h"
+#include "Tools/Geometry.h"
+
+
+using namespace glm;
 
 void RenderManager :: renderLoop(){
 	std::cout<<"renderLoop()..."<<std::endl; 
