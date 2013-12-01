f526ca3f420a7f75784fd723defb6f8deb8259c4
RuuNa <qala@uni-koblenz.de>
2013-12-01 21:42:59 +0100

    Renderer WIP: RenderQueue List Error

--- src/libraries/Visuals/RenderQueue.h
+++ src/libraries/Visuals/RenderQueue.h
@@ -1,11 +1,17 @@
 #ifndef RENDERQUEUE_H
 #define RENDERQUEUE_H
 
+#include <list>
+
 #include "VirtualObject.h"
-#include "Patterns/Singleton.h"
+#include "Visuals/RenderManager.h"
+
+using namespace std;
 
 class RenderQueue {
 // List of VirtualObjects possibly via HashMap
+private:
+	list<VirtualObject*> voList;
 };
 
 #endif /* RENDERQUEUE_H */
