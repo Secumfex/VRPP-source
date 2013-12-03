ecaa6eb0d404ee7f66ba9f8cd138e14662881530
BatsiMcLovin <kreuzgar@yahoo.de>
2013-12-03 18:20:39 +0100

    StateMachine soll der neue Context werden
    Anstatt dem Context nennen wir das jetzt StateMachine, damit es zu
    weniger verwirrung kommt...

---
+++ src/libraries/Patterns/StateMachine.cpp
@@ -0,0 +1,18 @@
+/*
+ * StateMachine.cpp
+ *
+ *  Created on: 03.12.2013
+ *      Author: Batsi
+ */
+
+#include <Patterns/StateMachine.h>
+
+StateMachine::StateMachine() {
+	// TODO Auto-generated constructor stub
+
+}
+
+StateMachine::~StateMachine() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/libraries/Patterns/StateMachine.h
@@ -0,0 +1,17 @@
+/*
+ * StateMachine.h
+ *
+ *  Created on: 03.12.2013
+ *      Author: Batsi
+ */
+
+#ifndef STATEMACHINE_H_
+#define STATEMACHINE_H_
+
+class StateMachine {
+public:
+	StateMachine();
+	virtual ~StateMachine();
+};
+
+#endif /* STATEMACHINE_H_ */
