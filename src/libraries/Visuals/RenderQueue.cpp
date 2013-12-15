5ea8b94b63faf5821e9508a1f495ac9a4d5ac802
RuuNa <qala@uni-koblenz.de>
2013-12-15 19:16:33 +0100

    RenderQueue: cleanup

--- src/libraries/Visuals/RenderQueue.cpp
+++ src/libraries/Visuals/RenderQueue.cpp
@@ -44,18 +44,11 @@ void RenderQueue::resetQueue(){
 	currentFirstElement = voList.begin();
 }
 
-/*
-Working with all the VOs from voList. Thus, need to iterate the list. While doing so we need to extract GCs from each VO.
-After extracting the GC vectors need to be sorted into the map using their tag. Currently no tag exists. in the map there
-are several lists each containing vectors of GCs. Each list specifies a certain shader and each vector is extractet from a 
-single VO.
-*/
+//extract VOs from voList, extract gc-vectors from the VOs, extract the gcs from the vectors and throw them into a list in a map
 void RenderQueue::extractAndSort(){
 	string shader = "DEFERRED_SHADING"; 
 	VirtualObject* vo;
 	vector<GraphicsComponent>* gcVector;
-	map<string, vector<GraphicsComponent*> >::iterator i;
-	GraphicsComponent gc;
 
 	while(hasNext()){
 		vo = getNextObject();
