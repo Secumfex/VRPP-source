e2ae317c197a4cf249b16753316049b0cd6cd54b
traubenzucker3001 <traubenzucker3001@googlemail.com>
2013-12-16 22:02:26 +0100

    doxygen test
    - die Physik betreffenden Header zum Test dokumentiert
    * still needs fixes

--- src/libraries/Physics/PhysicWorld.h
+++ src/libraries/Physics/PhysicWorld.h
@@ -2,20 +2,29 @@
 #define PHYSICWORLD_H_
 
 #include "btBulletDynamicsCommon.h"
-//#include "bullet.h"
 
 class PhysicWorld {
 public:
 
-	btBroadphaseInterface* broadphase;
-	btDefaultCollisionConfiguration* collisionConfiguration;
-	btCollisionDispatcher* dispatcher;
-	btSequentialImpulseConstraintSolver* solver;
-	btDiscreteDynamicsWorld* dynamicsWorld;
+	btBroadphaseInterface* broadphase;							/**< specifies the broadphase algorithm */
+	btDefaultCollisionConfiguration* collisionConfiguration;	/**< fine tunes the algorithms used for the full (not broadphase) collision detection */
+	btCollisionDispatcher* dispatcher;							/**< register a callback that filters overlapping broadphase proxies */
+	btSequentialImpulseConstraintSolver* solver;				/**< causes the objects to interact properly */
+	btDiscreteDynamicsWorld* dynamicsWorld;						/**< bullet dynamic world */
 
 	//void initPhysics();
 	//void deleteLoc();
+
+	/** \brief constructor
+	 *
+	 * initializes the physics world
+	 */
 	PhysicWorld();
+
+	/** \brief destructor
+	 *
+	 * clears the allocated memory, deletes dynamicsWorld, solver, collisionConfiguration, dispatcher, broadphase
+	 */
 	~PhysicWorld();
 
 	//bool collisionCallbackFunc(btManifoldPoint& collisionPoint, const btCollisionObjectWrapper* obj1, int id1, int index1, const btCollisionObjectWrapper* obj2, int id2, int index2);
--- src/libraries/Visuals/VirtualObject.h
+++ src/libraries/Visuals/VirtualObject.h
@@ -10,18 +10,37 @@
 class VirtualObject{
 public:
 
-	//TODO Model Matrix
-	glm::mat4 modelMatrix;
+	glm::mat4 modelMatrix;	/**< 4x4Matrix */
 
-	int id;
+	int id;					/**< identification number */
 
-	PhysicsComponent *physicsComponent;
-	vector<GraphicsComponent> graphicsComponent;
+	PhysicsComponent *physicsComponent; 			/**< pointer to the physics component of the VO */
+	vector<GraphicsComponent> graphicsComponent;	/**< pointer to the graphics Component of the VO*/
 
+	/** \brief constructor
+	 *
+	 * void constructor, sets modelMatrix to zero, sets the id to last id+1 and creates a new physicsComponent.
+	 */
 	VirtualObject();
+
+	/** \brief constructor
+	 *
+	 * if you know a modelMAtrix you can start with this constructor.
+	 * it sets modelMatrix to the given one, sets the id to last id+1  and creates a new physicsComponent.
+	 * @param modelMatrix 4x4Matrix
+	 */
 	VirtualObject(glm::mat4 modelMatrix);
+
+	/** \brief destructor
+	 *
+	 * deletes the physics and graphics components
+	 */
 	~VirtualObject();
 
+	/** \brief updates the modelMatrix
+	 *
+	 * fetches the current modelMatrix of the physicsComponent
+	 */
 	void updateModelMatrix();
 };
 
