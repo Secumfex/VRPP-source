f17344c31e4aba306892eeda03f12ffd4b583df7
Secumfex <Arend.Buchacher@gmx.de>
2013-12-17 10:55:32 +0100

    Bullet Cmake Files - MinGW fix

--- cmake/FindBullet.cmake
+++ cmake/FindBullet.cmake
@@ -11,23 +11,72 @@ IF (MINGW)
 	FIND_PATH( BULLET_INCLUDE_PATH bullet.h
 		${DEPENDENCIES_PATH}/bullet/src 
 	)
-    FIND_LIBRARY( BULLET_LIBRARY
-        NAMES libBulletFileLoader  libBulletXmlWorldImporter libConvexDecomposition libGIMPACTUtils libHACD libOpenGLSupport libBulletWorldImporter libBulletSoftBody libBulletDynamics libBulletCollision libLinearMath 
+    FIND_LIBRARY( l1
+        NAMES libBulletFileLoader 
         PATHS
         ${DEPENDENCIES_PATH}/bullet/lib_mingw
     )
 
+    FIND_LIBRARY( l2
+        NAMES  libBulletXmlWorldImporter 
+        PATHS
+        ${DEPENDENCIES_PATH}/bullet/lib_mingw
+    )
+
+    FIND_LIBRARY( l3
+        NAMES  libConvexDecomposition 
+        PATHS
+        ${DEPENDENCIES_PATH}/bullet/lib_mingw
+    )
+
+    FIND_LIBRARY( l4
+        NAMES  libBulletWorldImporter 
+        PATHS
+        ${DEPENDENCIES_PATH}/bullet/lib_mingw
+    )
+
+    FIND_LIBRARY( l5
+        NAMES  libBulletSoftBody
+        PATHS
+        ${DEPENDENCIES_PATH}/bullet/lib_mingw
+    )
+
+    FIND_LIBRARY( l6
+        NAMES  libOpenGLSupport 
+        PATHS
+        ${DEPENDENCIES_PATH}/bullet/lib_mingw
+    )
+
+    FIND_LIBRARY( l7
+        NAMES  libBulletDynamics 
+        PATHS
+        ${DEPENDENCIES_PATH}/bullet/lib_mingw
+    )
+
+    FIND_LIBRARY( l8
+        NAMES  libBulletCollision 
+        PATHS
+        ${DEPENDENCIES_PATH}/bullet/lib_mingw
+    )
+
+    FIND_LIBRARY( l9
+        NAMES  libLinearMath 
+        PATHS
+        ${DEPENDENCIES_PATH}/bullet/lib_mingw
+    )
+
+    set(BULLET_LIBRARY ${l1} ${l2} ${l3} ${l4} ${l5} ${l6} ${l7} ${l8} ${l9})
+
 ELSEIF (MSVC)
-	FIND_PATH( BULLET_INCLUDE_PATH bullet.h
-		${DEPENDENCIES_PATH}/bullet/src 
-	)
+    FIND_PATH( BULLET_INCLUDE_PATH bullet.h
+        ${DEPENDENCIES_PATH}/bullet/src 
+    )
 
     FIND_LIBRARY( BULLET_LIBRARY
         NAMES ConvexDecomposition GIMPACTUtils BulletFileLoader BulletMuliThreaded MiniCL BulletWorldImporter BulletXmlWorldImporter BulletSoftBody BulletSoftBodySolvers_OpenCL_MiniHACD BulletDynamics BulletCollision LinearMath
         PATHS
         ${DEPENDENCIES_PATH}/bullet/lib_vs
     )
-
 
 ELSEIF(APPLE)
 
