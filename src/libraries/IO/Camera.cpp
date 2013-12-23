5c5bb68f00aee1ed7e7c9a74acabc23b1d349a28
Secumfex <Arend.Buchacher@gmx.de>
2013-12-23 15:35:19 +0100

    Camera : additions / Doxygen-comments
    some additions to the Camera class
    doxygen documentation

--- src/libraries/IO/Camera.cpp
+++ src/libraries/IO/Camera.cpp
@@ -1,16 +1,33 @@
 #include "Camera.h"
-
-#define PI 3.14159265
-
+/**
+ * @param pos 		position in world coordinates
+ * @param direction view direction of camera
+ * @param up 		up vector
+ */
 Camera::Camera(glm::vec3 pos, glm::vec3 direction, glm::vec3 up){ 
 	m_position 	= pos,
-	m_direction = direction;
-	m_up 		= up;
+    m_up 		= up;
+
+	m_direction = glm::normalize(direction);
 	
-	//TODO : compute theta, phi by using the direction vector
-	m_theta 	= PI/2.0; 	//horizontal
-	m_phi 		= 3.0*PI/2.0;	  //gerade aus in z=-1 richtung	
-	}
+	updatePolarCoordinates();
+}
+
+/**
+ * @param pos 		position in world coordinates
+ * @param theta 	pitch - rotation about x-axis
+ * @param phi	 	yaw   - rotation about y-axis
+ * @param up 		up vector
+ */
+Camera::Camera(glm::vec3 pos, float theta, float phi, glm::vec3 up){
+	m_position 	= pos,
+	m_up 		= up;
+
+	m_theta 	= theta;
+	m_phi 		= phi;
+
+	updateViewDirection();
+}
 
 glm::mat4 Camera::getViewMatrix(){
 	glm::vec3 pointofinterest = m_position + m_direction;
@@ -18,10 +35,15 @@ glm::mat4 Camera::getViewMatrix(){
 }
 
 void Camera::updateViewDirection(){
-	m_direction = glm::vec3(std::sin(m_theta)*std::cos(m_phi),
-				std::cos(m_theta),
-		   std::sin(m_phi)*std::sin(m_theta));
+	m_direction = glm::vec3(std::sin(m_theta) * std::cos(m_phi),
+							std::cos(m_theta),
+							std::sin(m_phi)   * std::sin(m_theta));
 	glm::normalize(m_direction);
+}
+
+void Camera::updatePolarCoordinates(){
+	m_theta = std::acos( m_direction.z);
+	m_phi   = std::atan2((float) m_direction.y , (float) m_direction.x);
 }
 
 Camera::~Camera(){}
@@ -77,6 +99,14 @@ void Camera::moveForward(float distance){
 	move(translation); 
 }
 
+void Camera::moveBackward(float distance){
+	moveForward(-distance);
+}
+
+void Camera::moveRight(float distance){
+	moveLeft(-distance);
+}
+
 void Camera::moveLeft(float distance){
 	glm::vec3 translation = glm::normalize(glm::cross(m_up, m_direction)) * distance;
 	move(translation);
@@ -84,4 +114,8 @@ void Camera::moveLeft(float distance){
 
 void Camera::moveUp(float distance){
 	glm::vec3 translation = m_up * distance;
+}
+
+void Camera::moveDown(float distance){
+	moveUp(-distance);
 }
--- src/libraries/IO/Camera.h
+++ src/libraries/IO/Camera.h
@@ -5,28 +5,42 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <cmath>
 
-/*First Person Kamera*/
+#ifndef PI
+#define PI 3.14159265
+#endif
+
+///A Camera can be used for first person view. It is represented by a position and view direction.
+/**
+ *	The view direction is computed by the polar coordinates phi and theta.
+ *	The camera can be turned by using the turn method
+ *	A view matrix can be computed.
+ */
 class Camera{
 private:
 	glm::vec3 m_position;
 	glm::vec3 m_direction;
 	glm::vec3 m_up;
 
-	float m_phi;		//Drehung
-	float m_theta;		//Neigung
+	float m_phi;		///< yaw	:  rotation about y-axis
+	float m_theta;		///< pitch : rotation about x-axis
 
-inline void updateViewDirection();	// Blickrichtung aus Winkeln berechnen 
+inline void updateViewDirection();		// compute and save direction vector  from polar coordinates
+inline void updatePolarCoordinates();	// compute and save polar coordinates from direction vector
 
 public:
 	Camera(glm::vec3 pos = glm::vec3(0.0,0.0,0.0), glm::vec3 direction = glm::vec3(0.0,0.0,-1.0), glm::vec3 up = glm::vec3(0.0,1.0,0.0));
+	Camera(glm::vec3 pos = glm::vec3(0.0,0.0,0.0), float phi = 3.0*PI/2.0, float theta = PI/2.0,  glm::vec3 up = glm::vec3(0.0,1.0,0.0));
 	~Camera();
 
-	void turn(float deg_phi, float deg_theta);	// Kamera drehen
-	void move(float delta_x, float delta_y, float delta_z);	// Kamera bewegen
-	void move(glm::vec3 translation);	// Kamera bewegen
-	void moveForward(float distance);	//Kamera entlang Blickrichtung bewegen
-	void moveLeft(float distance);		//Kamera Horizontal zur Blickrichtung bewegen 
-	void moveUp(float distance);		//Kamera entlang Up-Vektor verschieben -- NICHT VERTIKAL ZU BLICKRICHTUNG
+	void turn(float deg_phi, float deg_theta);				//!< turn camera by degrees vertically, horizontally
+	void move(float delta_x, float delta_y, float delta_z);	//!< move camera
+	void move(glm::vec3 translation);		//!< move camera along translation vector
+	void moveForward(float 	distance);		//!< move camera along direction vector
+	void moveBackward(float 	distance);	//!< move camera along negative direction vector
+	void moveLeft	(float 	distance);		//!< move camera orthogonally to direction to the left
+	void moveRight	(float 	distance);		//!< move camera orthogonally to direction to the right
+	void moveUp		(float	distance);		//!< move camera along up-vector (NOTE: not necessarily orthogonally to direction)
+	void moveDown	(float	distance);		//!< move camera along negative up-vector (NOTE: not necessarily orthogonally to direction)
 
 	void setPosition(float x, float y, float z);
 	glm::vec3 getPosition();
@@ -34,7 +48,7 @@ public:
 	float getPhi();
 	float getTheta();
 
-	glm::mat4 getViewMatrix();	// View Matrix berechnen und zurück lassen
+	glm::mat4 getViewMatrix();	///< compute view matrix from position and direction vectors
 };
 
 #endif
