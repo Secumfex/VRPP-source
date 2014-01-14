3f4cddc42cc51dcd2c8bf36e6371a7ff5cfd5053
TreCool90 <oliverplauk@uni-koblenz.de>
2014-01-14 09:25:10 +0100

    Erster Entwurf IO

--- src/libraries/IO/IOManager.cpp
+++ src/libraries/IO/IOManager.cpp
@@ -1,5 +1,81 @@
 #include "IOManager.h"
 
+IOManager::IOManager(){
+	/* STATE CHECK */
+	//TODO implement state check and change
+	isMenuState = false;
+
+	//TODO discuss if implement here or get from Rendermanager
+	//initialFoV = 45.0f;
+
+	speed = 3.0f; // 3 units / second
+	mouseSpeed = 0.005f;
+
+	// Get mouse position
+	glfwGetCursorPos(window, &xPos, &yPos);
+}
+
+/*
+void IOManager::computeFoV(){
+	FoV = initialFoV - 5 * glfwGetMouseWheel();
+}
+*/
+
+void IOManager::computeFrameTimeDifference(){
+	// glfwGetTime is called only once, the first time this function is called
+	lastTime = glfwGetTime();
+
+	// Compute time difference between current and last frame
+	currentTime = glfwGetTime();
+	deltaTime = float(currentTime - lastTime);
+}
+
+// Compute new orientation
+void IOManager::setOrientation(){
+	// Reset mouse position for next frame
+	glfwSetCursorPos(window, WIDTH / 2, HEIGHT / 2);
+
+	float gotPhi = camObject->getPhi();
+	float gotTheta = camObject->getTheta();
+	camObject->setPhi(gotPhi += mouseSpeed * float(WIDTH / 2 - xPos));
+	camObject->setTheta(gotTheta += mouseSpeed * float(HEIGHT / 2 - yPos));
+}
+
+void IOManager::computeIO(){
+	computeFrameTimeDifference();
+
+	setOrientation();
+
+	// For the next frame, the "last time" will be "now"
+	lastTime = currentTime;
+}
+//TODO switch implementations for menu state and game state
+void IOManager::keyFunction(int key){
+	glm::vec3 gotPosition = camObject->getPosition();
+	// Move forward
+	switch (key){
+		case glfwGetKey(window, GLFW_KEY_UP):
+			gotPosition += camObject->getViewDirection() * deltaTime * speed;
+			camObject->setPosition(gotPosition);
+			break;
+		// Move backward
+		case glfwGetKey(window, GLFW_KEY_DOWN):
+			gotPosition -= camObject->getViewDirection() * deltaTime * speed;
+			camObject->setPosition(gotPosition);
+			break;
+		// Strafe right
+		case glfwGetKey(window, GLFW_KEY_RIGHT):
+			gotPosition += camObject->getRight() * deltaTime * speed;
+			camObject->setPosition(gotPosition);
+			break;
+		// Strafe left
+		case glfwGetKey(window, GLFW_KEY_LEFT):
+			gotPosition -= camObject->getRight() * deltaTime * speed;
+			camObject->setPosition(gotPosition);
+			break;
+	}
+}
+
 glm::mat4 IOManager::getViewMatrix(){
 	return mViewMatrix;
 }
--- src/libraries/IO/IOManager.h
+++ src/libraries/IO/IOManager.h
@@ -1,14 +1,52 @@
 #ifndef IOMANAGER_H
 #define IOMANAGER_H
 
+#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include "Camera.h"
 
+//TODO make singleton
+/*class IOManager : public Singleton<IOManager>{
+	friend class Singleton<IOManager>;*/
 class IOManager{
 
 private:
+	/* WINDOW SIZE */
+	// TODO get window size from rendermanager
+	int WIDTH, HEIGHT;
+
+	/* STATE CHECK */
+	// TODO implement state check and change
+	bool isMenuState;
+
+	//TODO discuss if implement here or get from Rendermanager
+	//float initialFoV;
+
+	// mouse position
+	double xPos, yPos;
+
+	float speed;
+	float mouseSpeed;
+
 	glm::mat4 mViewMatrix;
+
+	static double lastTime;
+	double currentTime;
+	float deltaTime;
+
+	inline void setOrientation();
+
+	inline void computeFrameTimeDifference();
+
+	Camera* camObject = new Camera();
+
 public:
+	//float FoV;
+	GLFWwindow* window;
 	glm::mat4 getViewMatrix();
+	void computeIO();
+	void keyFunction(int key);
+	//void computeFoV();
 };
 
 #endif
