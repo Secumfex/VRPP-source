09e88754f4baa69affe121ac18640b5c3b82e4f5
raphimulator <rheidrich@uni-koblenz.de>
2014-01-15 19:22:28 +0100

    grrr_nichts_klappt

--- src/executables/Resource_Test/main.cpp
+++ src/executables/Resource_Test/main.cpp
@@ -20,6 +20,8 @@
 #include "Tools/ShaderTools.h"
 #include "Tools/TextureTools.h"
 
+#include "Tools/Geometry.h"
+
 int main() {
     // render window
     glfwInit();
@@ -33,16 +35,14 @@ int main() {
 #endif
 
 
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Compositing", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 500, "Resource_Test", NULL, NULL);
     glfwMakeContextCurrent(window);
-    glClearColor(1,0,0,0);
+    glewInit();
+    glClearColor(0,0,0.5,0);
 
-    // get framebuffer size
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
 
     //init opengl 3 extension
-    glewInit();
+
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
     std::cout << "GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
     std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
@@ -52,9 +52,9 @@ int main() {
 
 
     GLuint phongShaderHandle = ShaderTools::makeShaderProgram(
-            SHADERS_PATH "/Phong/phong.vert",
-            SHADERS_PATH "/Phong/phong.frag");
-    glEnable(GL_DEPTH);
+            SHADERS_PATH "/Phong_Test/phong.vert",
+            SHADERS_PATH "/Phong_Test/phong.frag");
+    glEnable(GL_DEPTH_TEST);
 
     GLuint modelHandle = glGetUniformLocation(phongShaderHandle, "uniformModel");
     GLuint viewHandle = glGetUniformLocation(phongShaderHandle, "uniformView");
@@ -63,40 +63,69 @@ int main() {
 
 
     //der Order muss im System existieren
-    	VirtualObjectFactory* voFactory = VirtualObjectFactory::getInstance();
-    	VirtualObject* cow = voFactory->createVirtualObject(RESOURCES_PATH "/cow.obj");
+//    	VirtualObjectFactory* voFactory = VirtualObjectFactory::getInstance();
+//    	VirtualObject* cow = voFactory->createVirtualObject(RESOURCES_PATH "/cow.obj");
+
+        GLuint cubeVertexArrayHandle;
+        {
+            glGenVertexArrays(1, &cubeVertexArrayHandle);
+            glBindVertexArray(cubeVertexArrayHandle);
+
+            //we generate multiple buffers at a time
+            GLuint vertexBufferHandles[3];
+            glGenBuffers(3, vertexBufferHandles);
+
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBufferHandles[0]);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(CubeGeometry::positions), CubeGeometry::positions, GL_STATIC_DRAW);
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBufferHandles[1]);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(CubeGeometry::uvCoordinates), CubeGeometry::uvCoordinates, GL_STATIC_DRAW);
+            glEnableVertexAttribArray(1);
+            glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBufferHandles[2]);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(CubeGeometry::normals), CubeGeometry::normals, GL_STATIC_DRAW);
+            glEnableVertexAttribArray(2);
+            glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        }
+
+        cout<<cubeVertexArrayHandle<<endl;
 
     	using namespace glm;
 
         mat4 modelMatrix = mat4(1.0f);
 
-        mat4 viewMatrix = lookAt(vec3(0.0f, 1.0f, -6.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
+        mat4 viewMatrix = lookAt(vec3(0.0f, 0.0f, -6.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
         mat4 projectionMatrix = perspective(40.0f, 4.0f / 3.0f, 0.1f, 100.f);
 
-        mat4 inverseMatrix = transpose(inverse(modelMatrix * viewMatrix));
+        mat4 inverseMatrix = transpose(inverse(viewMatrix * modelMatrix));
 
         glUniformMatrix4fv(modelHandle, 1, GL_FALSE, value_ptr(modelMatrix));
         glUniformMatrix4fv(viewHandle, 1, GL_FALSE, value_ptr(viewMatrix));
         glUniformMatrix4fv(projectionHandle, 1, GL_FALSE, value_ptr(projectionMatrix));
         glUniformMatrix4fv(inverseHandle, 1, GL_FALSE, value_ptr(inverseMatrix));
 
-        glUseProgram(phongShaderHandle);
 
     	 while(!glfwWindowShouldClose(window)) {
 
-    		 std::cout <<"das programm kommt bis zur schleife" <<std::endl;
+    	       glUseProgram(phongShaderHandle);
 
-    	        glfwMakeContextCurrent(window);
+ 	        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    	        unsigned int i = 0;
-    	        for (i = 0; i < cow->getGraphicsComponent().size(); ++i) {
-        	        glBindVertexArray(cow->getGraphicsComponent()[i]->getMesh()->getVAO());
-        	        glDrawArrays(GL_TRIANGLES, 0, cow->getGraphicsComponent()[i]->getMesh()->getNumFaces() );
-				}
+//    	        unsigned int i = 0;
+//    	        for (i = 0; i < cow->getGraphicsComponent().size(); ++i) {
+//        	        glBindVertexArray(cow->getGraphicsComponent()[i]->getMesh()->getVAO());
+//        	        glDrawArrays(GL_TRIANGLES, 0, cow->getGraphicsComponent()[i]->getMesh()->getNumFaces() );
+//    				cout<<"Kuh Nr."<<i<<" wird gezeichnet!"<<endl;
+//    	        }
+				glBindVertexArray(cubeVertexArrayHandle);
+    	        glDrawArrays(GL_TRIANGLES, 0, 12*3);
+//    				cout<<"Cube wird gezeichnet"<<endl;
 
+      	        glfwPollEvents();
     	        glfwSwapBuffers(window);
-    	        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    	        glfwPollEvents();
     	 }
     	    glfwDestroyWindow(window);
     	    glfwTerminate();
---
+++ src/shaders/Phong_Test/phong.frag
@@ -0,0 +1,35 @@
+#version 330 
+
+in vec3 passNormal;
+in vec3 passPosition;
+in vec3 passLightPosition;
+
+out vec4 fragmentColor;
+
+void main() { 
+    //compute the light vector as the normalized vector between 
+    //the vertex position and the light position:
+    vec3 lightVector = normalize(passLightPosition - passPosition);
+    float distance = dot((passLightPosition - passPosition),(passLightPosition - passPosition));
+
+    //compute the eye vector as the normalized negative vertex 
+    //position in camera coordinates:
+    vec3 eye = normalize(-passPosition);
+    
+    //compute the normalized reflection vector using 
+    //GLSL's built-in reflect() function:
+    vec3 reflection = normalize(reflect(-lightVector, passNormal));
+
+    //comment in to use the phong lighting model:
+    float diffuse = (max(dot(passNormal, lightVector), 0) / distance) *15.0;
+    float specular = (pow(max(dot(reflection, eye), 0), 15) / distance) *10.0;
+    float ambient = 0.3;
+
+    //fragmentColor = vec4(
+    //comment in to use the phong lighting model:
+      //  diffuse  * vec3(1, 0, 0) + 
+       // specular * vec3(1, 1, 1) + 
+       // ambient  * vec3(1, 0.5, 0.5), 
+       // 1);
+       fragmentColor = vec4(1,1,0,1);
+}
