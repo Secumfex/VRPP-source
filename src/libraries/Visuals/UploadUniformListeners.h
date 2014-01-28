8949a7cc453b41d3b4c278954be060c9ae740d48
raphimulator <rheidrich@uni-koblenz.de>
2014-01-28 10:36:33 +0100

    UniformListener braucht keinen eigenen RM

--- src/libraries/Visuals/UploadUniformListeners.h
+++ src/libraries/Visuals/UploadUniformListeners.h
@@ -2,50 +2,43 @@
 #define UPLOADUNIFORMLISTENERS_H_
 
 class UploadUniformModelMatrixListener : public Listener{
- 	private:
- 		RenderManager* rm;
+
  	public:
  	 	void update();
  };
 
  class UploadUniformViewMatrixListener : public Listener{
- 	private:
- 		RenderManager* rm;
+
  	public:
  	 	void update();
  };
 
  class UploadUniformProjectionMatrixListener : public Listener{
- 	private:
- 		RenderManager* rm;
+
  	public:
  	 	void update();
  };
 
  class UploadUniformInverseModelViewMatrixListener : public Listener{
- 	private:
- 		RenderManager* rm;
+
  	public:
  	 	void update();
  };
 
  class UploadUniformPositionMapListener : public Listener{
- 	private:
- 		RenderManager* rm;
+
  	public:
  	 	void update();
  };
 
  class UploadUniformNormalMapListener : public Listener{
- 	private:
- 		RenderManager* rm;
+
  	public:
  	 	void update();
  };
 
  class UploadUniformColorMapListener : public Listener{
- 	private:
- 		RenderManager* rm;
+
  	public:
  	 	void update();
  };
