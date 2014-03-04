6d3dabac75a04f47c8a234bfc2f38f0c7b7a7eb9
deJuli <juliankarst@uni-koblenz.de>
2014-03-04 17:07:28 +0100

    pseudo_wall

--- src/executables/Wall_destruction/main.cpp
+++ src/executables/Wall_destruction/main.cpp
@@ -41,30 +41,98 @@ void configureMyApp(){
 	myVRState-> 	attachListenerOnActivation(			new PrintCameraStatusListener( myVRState->getCamera()));
 
 	/*	customize virtual objects*/
+	VirtualObject* 	myCubeObject1 = 	VirtualObjectFactory::getInstance()->createVirtualObject(RESOURCES_PATH "/cube.obj");	// create a Virtual Object by using the VirtualObject-Factory and add it to VRState manually
+	myVRState->		addVirtualObject(	myCubeObject1);		// add to VRState manually
+	VirtualObject* 	myCubeObject2 = 	myVRState->	createVirtualObject(RESOURCES_PATH "/cube.obj");	// create another Virtual Object from the same geometry
 
-	//VirtualObject* cube1 = myVRState-> createVirtualObject(RESOURCES_PATH "/cube.obj");
+	VirtualObject* 	cube1 = 	myVRState->	createVirtualObject(RESOURCES_PATH "/cube.obj");
+	VirtualObject* 	cube2 = 	myVRState->	createVirtualObject(RESOURCES_PATH "/cube.obj");
+	VirtualObject* 	cube3 = 	myVRState->	createVirtualObject(RESOURCES_PATH "/cube.obj");
+	VirtualObject* 	cube4 = 	myVRState->	createVirtualObject(RESOURCES_PATH "/cube.obj");
+	VirtualObject* 	cube5 = 	myVRState->	createVirtualObject(RESOURCES_PATH "/cube.obj");
+	VirtualObject* 	cube6 = 	myVRState->	createVirtualObject(RESOURCES_PATH "/cube.obj");
+	VirtualObject* 	cube7 = 	myVRState->	createVirtualObject(RESOURCES_PATH "/cube.obj");
+	VirtualObject* 	cube8 = 	myVRState->	createVirtualObject(RESOURCES_PATH "/cube.obj");
+	VirtualObject* 	cube9 = 	myVRState->	createVirtualObject(RESOURCES_PATH "/cube.obj");
+	VirtualObject* 	cube10 = 	myVRState->	createVirtualObject(RESOURCES_PATH "/cube.obj");
+	VirtualObject* 	cube11 = 	myVRState->	createVirtualObject(RESOURCES_PATH "/cube.obj");
+	VirtualObject* 	cube12 = 	myVRState->	createVirtualObject(RESOURCES_PATH "/cube.obj");
 
-	//VirtualObject* 	myCubeObject1 = 	VirtualObjectFactory::getInstance()->createVirtualObject(RESOURCES_PATH "/cube.obj");	// create a Virtual Object by using the VirtualObject-Factory and add it to VRState manually
-	//myVRState->		addVirtualObject(	myCubeObject1);		// add to VRState manually
-	//VirtualObject* 	myCubeObject2 = 	myVRState->	createVirtualObject(RESOURCES_PATH "/cube.obj");	// create another Virtual Object from the same geometry
+	glm::mat4		cube1M = 	glm::translate(glm::mat4(1.0f), glm::vec3(-2.5f, -1.0f, 0.0f));
+	cube1->getPhysicsComponent()->~PhysicsComponent();
+	cube1-> setModelMatrix(		cube1M); 	// override default Model Matrix
+	cube1->	setPhysicsComponent(1.0f, 1.0f, 1.0f, -2.5f, -1.0f, 0.0f, 1.0f);
+
+	glm::mat4		cube2M = 	glm::translate(glm::mat4(1.0f), glm::vec3(-1.5f, -1.0f, 0.0f));
+	cube2->getPhysicsComponent()->~PhysicsComponent();
+	cube2-> setModelMatrix(		cube2M); 	// override default Model Matrix
+	cube2->	setPhysicsComponent(1.0f, 1.0f, 1.0f, -1.5f, -1.0f, 0.0f, 1.0f);
+
+	glm::mat4		cube3M = 	glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f, -1.0f, 0.0f));
+	cube3->getPhysicsComponent()->~PhysicsComponent();
+	cube3-> setModelMatrix(		cube3M); 	// override default Model Matrix
+	cube3->	setPhysicsComponent(1.0f, 1.0f, 1.0f, -0.5f, -1.0f, 0.0f, 1.0f);
+
+	glm::mat4		cube4M = 	glm::translate(glm::mat4(1.0f), glm::vec3(0.5f, -1.0f, 0.0f));
+	cube4->getPhysicsComponent()->~PhysicsComponent();
+	cube4-> setModelMatrix(		cube4M); 	// override default Model Matrix
+	cube4->	setPhysicsComponent(1.0f, 1.0f, 1.0f, 0.5f, -1.0f, 0.0f, 1.0f);
+
+	////////////////////////////////////////////////////////////////////////////////////////
+
+	glm::mat4		cube5M = 	glm::translate(glm::mat4(1.0f), glm::vec3(-2.5f, 0.0f, 0.0f));
+	cube5->getPhysicsComponent()->~PhysicsComponent();
+	cube5-> setModelMatrix(		cube5M); 	// override default Model Matrix
+	cube5->	setPhysicsComponent(1.0f, 1.0f, 1.0f, -2.5f, 0.0f, 0.0f, 1.0f);
+
+	glm::mat4		cube6M = 	glm::translate(glm::mat4(1.0f), glm::vec3(-1.5f, 0.0f, 0.0f));
+	cube6->getPhysicsComponent()->~PhysicsComponent();
+	cube6-> setModelMatrix(		cube6M); 	// override default Model Matrix
+	cube6->	setPhysicsComponent(1.0f, 1.0f, 1.0f, -1.5f, 0.0f, 0.0f, 1.0f);
+
+	glm::mat4		cube7M = 	glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f, 0.0f, 0.0f));
+	cube7->getPhysicsComponent()->~PhysicsComponent();
+	cube7-> setModelMatrix(		cube7M); 	// override default Model Matrix
+	cube7->	setPhysicsComponent(1.0f, 1.0f, 1.0f, -0.5f, 0.0f, 0.0f, 1.0f);
+
+	glm::mat4		cube8M = 	glm::translate(glm::mat4(1.0f), glm::vec3(0.5f, 0.0f, 0.0f));
+	cube8->getPhysicsComponent()->~PhysicsComponent();
+	cube8-> setModelMatrix(		cube8M); 	// override default Model Matrix
+	cube8->	setPhysicsComponent(1.0f, 1.0f, 1.0f, 0.5f, 0.0f, 0.0f, 1.0f);
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+
+	glm::mat4		cube9M = 	glm::translate(glm::mat4(1.0f), glm::vec3(-2.5f, 1.0f, 0.0f));
+	cube9->getPhysicsComponent()->~PhysicsComponent();
+	cube9-> setModelMatrix(		cube9M); 	// override default Model Matrix
+	cube9->	setPhysicsComponent(1.0f, 1.0f, 1.0f, -2.5f, 1.0f, 0.0f, 1.0f);
+
+	glm::mat4		cube10M = 	glm::translate(glm::mat4(1.0f), glm::vec3(-1.5f, 1.0f, 0.0f));
+	cube10->getPhysicsComponent()->~PhysicsComponent();
+	cube10-> setModelMatrix(		cube10M); 	// override default Model Matrix
+	cube10->	setPhysicsComponent(1.0f, 1.0f, 1.0f, -1.5f, 1.0f, 0.0f, 1.0f);
+
+	glm::mat4		cube11M = 	glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f, 1.0f, 0.0f));
+	cube11->getPhysicsComponent()->~PhysicsComponent();
+	cube11-> setModelMatrix(		cube11M); 	// override default Model Matrix
+	cube11->	setPhysicsComponent(1.0f, 1.0f, 1.0f, -0.5f, 1.0f, 0.0f, 1.0f);
+
+	glm::mat4		cube12M = 	glm::translate(glm::mat4(1.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+	cube12->getPhysicsComponent()->~PhysicsComponent();
+	cube12-> setModelMatrix(		cube12M); 	// override default Model Matrix
+	cube12->	setPhysicsComponent(1.0f, 1.0f, 1.0f, 0.5f, 1.0f, 0.0f, 1.0f);
 
 
-	//cube1->translate(glm::vec3(1.0f, 1.5f, 1.0f));
-	//cube1->scale(glm::vec3(0.2f, 0.2f, 0.2f));
-
-	/* glm::mat4 		cube1Matrix = glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 1.5f, 1.0f)), glm::vec3(0.2f, 0.2f, 0.2f));
-	//cube1->			setModelMatrix( 	cube1Matrix);
-	//cube1->			setPhysicsComponent(0.2f, 0.2f, 0.2f, 1.0f, 1.5f, 1.0f, 1.0f); */
-	/*
-	glm::mat4		myModelMatrix1 = 	glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -1.0f, 0.0f)), glm::vec3(2.5f, 0.2f, 2.5f));	//floor
+	glm::mat4		myModelMatrix1 = 	glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -1.0f, 0.0f)), glm::vec3(5.0f, 0.2f, 5.0f));	//floor
 	myCubeObject1->getPhysicsComponent()->~PhysicsComponent();
 	myCubeObject1-> setModelMatrix(		myModelMatrix1); 	// override default Model Matrix
-	myCubeObject1->	setPhysicsComponent(2.5f, 0.2f, 2.5f, 0.0f, -1.0f, 0.0f, 0.0f);
+	myCubeObject1->	setPhysicsComponent(5.0f, 0.2f, 5.0f, 0.0f, -1.0f, 0.0f, 0.0f);
 
-	glm::mat4 myModelMatrix2 = glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -2.5f)), glm::vec3(2.5f, 2.5f, 0.2f));	// wall
+	glm::mat4 myModelMatrix2 = glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -2.5f)), glm::vec3(5.0f, 5.0f, 0.2f));	// wall
+	myCubeObject2->getPhysicsComponent()->~PhysicsComponent();
 	myCubeObject2-> setModelMatrix(		myModelMatrix2);	// override default Model Matrix
-	myCubeObject2-> setPhysicsComponent(2.5f, 2.5f, 0.2f, 0.0f, 0.0f, -2.5f, 0.0f);
-*/
+	myCubeObject2-> setPhysicsComponent(5.0f,5.0f, 0.2f, 0.0f, 0.0f, -2.5f, 0.0f);
+
 	/*	load some virtual objects into vr state scene*/	 		// create a Virtual Object by reading an .obj file and add it to VRState automatically
 	//PhysicsComponent* myCowObject1PhysicsComponent = 		myCowObject1->getPhysicsComponent();					// get PhysicsComponent pointer
 	//myVRState->		attachListenerOnBeginningProgramCycle( 	new UpdatePhysicsWorldListener());
@@ -105,7 +173,7 @@ void configureMyApp(){
 
 	/*	add customized states to application state pool*/
 	myApp->addState(	myMenu);		//add the Main Menu to Application
-	//myApp->addState(	myLoadingMenu);	//add the Loading Screen to Application
+	myApp->addState(	myLoadingMenu);	//add the Loading Screen to Application
 	myApp->addState(	myVRState);		//add the VR State to Application
 
 	myApp->setState(	"MAINMENU"); 	//set initial state to the state labeled MAINMENU
