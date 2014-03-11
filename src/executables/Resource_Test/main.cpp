ac1f268753e27133a228dc9022672f4befd5396b
BatsiMcLovin <kreuzgar@yahoo.de>
2014-03-11 14:43:26 +0100

    mini cow deleted
    commented the soda-object...

--- src/executables/Resource_Test/main.cpp
+++ src/executables/Resource_Test/main.cpp
@@ -54,7 +54,7 @@ int main() {
 
 	VirtualObjectFactory *voFactory = VirtualObjectFactory::getInstance();
 
-	VirtualObject *object01 = voFactory->createVirtualObject(RESOURCES_PATH "/soda_can1.obj");
+	//VirtualObject *object01 = voFactory->createVirtualObject(RESOURCES_PATH "/soda_can1.obj");
 	VirtualObject *object02 = voFactory->createVirtualObject(RESOURCES_PATH "/barrel.obj");
 	VirtualObject *object03 = voFactory->createVirtualObject(RESOURCES_PATH "/cube.obj");
     VirtualObject *object04 = voFactory->createVirtualObject(RESOURCES_PATH "/cow.obj");
@@ -91,7 +91,7 @@ int main() {
 	gbufferShader->setBlurStrength(0.5);
 	gbuffer_normalMap_Shader->setBlurStrength(0);
 
-	rq->addVirtualObject(object01);
+	//rq->addVirtualObject(object01);
 	rq->addVirtualObject(object02);
     rq->addVirtualObject(object04);
 	rq->addVirtualObject(object03);
@@ -140,7 +140,7 @@ int main() {
 
 		object03->setModelMatrix(modelMatrix01);
 		object02->setModelMatrix(modelMatrix02);
-		object01->setModelMatrix(modelMatrix03);
+		//object01->setModelMatrix(modelMatrix03);
         object04->setModelMatrix(modelMatrix04);
 
 		//--------------------------------------------//
@@ -153,7 +153,7 @@ int main() {
 		gbufferShader->useProgram();
 		rm->setCurrentShader(gbufferShader);
 		//----------------------------------------------------------------------------------------//
-		//        This is da Main-Renderloop. Hier werden alle GC für den GBuffer gerendert       //
+		//        This is da Main-Renderloop. Hier werden alle GC fï¿½r den GBuffer gerendert       //
 		//----------------------------------------------------------------------------------------//
 
 		list<VirtualObject*> vo_list = rm->getRenderQueue()->getVirtualObjectList();
@@ -190,7 +190,7 @@ int main() {
 		//      to compose them on an image plane     //
 		//--------------------------------------------//
 
-		//      Hier findet das Compositing statt :) ist schon einiges kürzer, nicht wahr?
+		//      Hier findet das Compositing statt :) ist schon einiges kï¿½rzer, nicht wahr?
 
 		finalCompShader->useProgram();
 		rm->setCurrentShader(finalCompShader);
