65d108ac28edf0128086ca416529ab6aec381369
traubenzucker3001 <traubenzucker3001@googlemail.com>
2014-03-12 13:39:04 +0100

    doxygen stuff - Physics
    - alle Physic-Klassen in einheitliche Doku-Form gebracht
    * scheint alles soweit dokumentiert zu sein

--- src/libraries/Physics/PhysicWorldSimulationListener.h
+++ src/libraries/Physics/PhysicWorldSimulationListener.h
@@ -7,12 +7,12 @@ class PhysicWorld;
 
 class PhysicWorldSimulationListener : public Listener {
 private:
-	float* delta_time_source;	/**< !docu pls! */
+	float* delta_time_source;	/**< fps, speed in which the physic simulation should run */
 public:
 
 	/** \brief listener constructor
 	 *
-	 * @param delta_time_source ?TODO
+	 * @param delta_time_source speed in which the physic simulation should run
 	 */
 	PhysicWorldSimulationListener(float* delta_time_source = 0);
 
