7b64b65d5838743d75f65d1b04b440fa214611bf
traubenzucker3001 <traubenzucker3001@googlemail.com>
2014-03-13 11:02:42 +0100

    doxygen stuff - Tools
    - alle Tools-Klassen in einheitliche Doku-Form gebracht
    * nur ein paar Kleinigkeiten die noch genauer dokumentiert werden
    könnten

--- src/libraries/Tools/NoAssimpVirtualObjectFactory.h
+++ src/libraries/Tools/NoAssimpVirtualObjectFactory.h
@@ -3,19 +3,36 @@
 
 #include "Visuals/VirtualObject.h"
 
-/// a workaround class which is able to create a generic cube Virtual Object without relying on Assimp libraries, for testing or trouble-shooting purposes
-
-/// a workaround class which is able to create a generic cube Virtual Object without relying on Assimp libraries, for testing or trouble-shooting purposes
-
+/** \brief a workaround class which is able to create a generic cube Virtual Object without relying on Assimp libraries, for testing or trouble-shooting purposes
+ *
+ */
 class NoAssimpVirtualObjectFactory{
 private:
-	GLuint createCubeVAO();		//!< create a VAO from Cube Geometry
+	/** \brief create a VAO from Cube Geometry
+	 *
+	 * @return GLuint
+	 */
+	GLuint createCubeVAO();
+
+	/** \brief create mesh by creating a VAO from Cube Geometry
+	 *
+	 * @return cubeMesh
+	 */
+	Mesh* createCubeMesh();
 	
-	Mesh* createCubeMesh();		//!< create mesh by creating a VAO from Cube Geometry
-	Material* createCubeMaterial(); //!< create material by loading some textures 
+	/** \brief create material by loading some textures
+	 *
+	 * @return cubeMaterial
+	 */
+	Material* createCubeMaterial();
 	
 public:
-	VirtualObject* createCubeObject(float mass = 0.0);	//!< create a Cube VirtualObject
+	/** \brief create a Cube VirtualObject
+	 *
+	 * @param mass defines the behavior of the rigid body in the physics world
+	 * @return a virtual object
+	 */
+	VirtualObject* createCubeObject(float mass = 0.0);
 };
 
 #endif
