a3b6703bcbb503ac6825b2ec4b58bee56ae51fd8
RuuNa <qala@uni-koblenz.de>
2014-03-15 18:13:37 +0100

    Hotfix via forward declaration

--- src/libraries/Patterns/Visitor.h
+++ src/libraries/Patterns/Visitor.h
@@ -1,9 +1,13 @@
 #ifndef VISITOR_H
 #define VISITOR_H
 
-#include "Visuals/RenderQueue.h"
-
 //using namespace std;
+
+/*! @brief Proxyclass for forward including
+ *
+ *
+ */
+class RenderQueue;
 
 /*! @brief Visitor is the super  class of more specific visitor.
  *
