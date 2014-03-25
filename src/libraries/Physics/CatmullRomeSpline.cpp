5cb6b43bf531213f9cf39a37ea52fa774776fa20
TreCool90 <oliverplauk@uni-koblenz.de>
2014-03-25 12:18:20 +0100

    Variablendeklaration

--- src/libraries/Physics/CatmullRomeSpline.cpp
+++ src/libraries/Physics/CatmullRomeSpline.cpp
@@ -1,10 +1,24 @@
-/*
- * CatmullRomeSpline.cpp
- *
- *  Created on: 25.03.2014
- *      Author: Flower
- */
+#include "CatmullRomeSpline.h"
+
+using namespace std;
+
+CatmullRomeSpline::CatmullRomeSpline(){
+	Seetang = new Seetang();
+	float Tk = 0.5;				 				//Krümmungsvariable der Kurve
+	float Ab = 0.1;								//Abstandsvariable im Spline
+	int RV0 = 0;								//Richtungsvektoren zum Vertexziehen
+	int RV1 = 1;
+}
+
+void CatmullRomeSpline::MVMultiplikation(){
+
+}
+
+void CatmullRomeSpline::Position(){
 
 
+}
 
+void CatmullRomeSpline::Value(){
 
+}
--- src/libraries/Physics/CatmullRomeSpline.h
+++ src/libraries/Physics/CatmullRomeSpline.h
@@ -1,13 +1,17 @@
-/*
- * CatmullRomeSpline.h
- *
- *  Created on: 25.03.2014
- *      Author: Flower
- */
+#include "../Feature_Fauna/Seetang.h"
 
 #ifndef CATMULLROMESPLINE_H_
 #define CATMULLROMESPLINE_H_
 
+class CatmullRomeSpline{
+private:
+	Seetang*  Seetang;		//Koordinaten vom Seetang, bestehend aus 4 Kugeln
+public:
+	void MVMultiplikation();
+	void Position();
+	void Value();
+
+};
 
 
 
