74f6e0bbf959081621b2e05582dc7beab0fa7e64
ddehrmann <ddehrmann@uni-koblenz.de>
2014-03-25 12:14:53 +0100

    next commit

--- src/executables/Feature_Fauna/main.cpp
+++ src/executables/Feature_Fauna/main.cpp
@@ -5,62 +5,72 @@
 #include "PlaceHolderListeners.h"
 
 /*
-*	A basic executable to use as starting point with our libraries
-*	see Demo-Application to see exemplary usage of listener interfaces, virtual object, input configuration and more
-*	tip: write short Listener classes to wrap code and attach to suitable listener interfaces; i.e. use attachListenerOnBeginningProgramCycle to use code during a program cycle
-*/
+ *	A basic executable to use as starting point with our libraries
+ *	see Demo-Application to see exemplary usage of listener interfaces, virtual object, input configuration and more
+ *	tip: write short Listener classes to wrap code and attach to suitable listener interfaces; i.e. use attachListenerOnBeginningProgramCycle to use code during a program cycle
+ */
 
-Application* 	testingApp;
-VRState* 		testingState;
-IOHandler*   	testingInputHandler;
+Application* testingApp;
+VRState* testingState;
+IOHandler* testingInputHandler;
 
-void configureTestingApplication(){
+void configureTestingApplication() {
 	/* customization of application or state*/
 	/* use listener interfaces for: what should happen on initialization, every program cycle, termination etc. */
-	testingApp->attachListenerOnProgramInitialization(	new PrintMessageListener(		string("Application is booting")));
-	testingApp->attachListenerOnProgramTermination(		new PrintMessageListener(		string("Application is terminating")));
+	testingApp->attachListenerOnProgramInitialization(
+			new PrintMessageListener(string("Application is booting")));
+	testingApp->attachListenerOnProgramTermination(
+			new PrintMessageListener(string("Application is terminating")));
 
 }
 
-void configureVirtualObjects(){
+void configureVirtualObjects() {
 	/* creation and customization of Virtual Objects */
 	/* use testingState->createVirtualObject() to create a Virtual Object */
 
+	testingState->createVirtualObject( RESOURCES_PATH "/aqua02.3ds",
+			VirtualObjectFactory::OTHER);
 }
 
-void configurePhysics(){
+void configurePhysics() {
 	/* customization of Bullet / Physicsworld */
 
 }
 
-void configureInputHandler(){
+void configureInputHandler() {
 	/* customization of input handling */
 	/* use listener interfaces for: what should happen when a specific key is pressed, etc. */
-	testingInputHandler->attachListenerOnKeyPress(		new TerminateApplicationListener(testingApp), GLFW_KEY_ESCAPE);
+	testingInputHandler->attachListenerOnKeyPress(
+			new TerminateApplicationListener(testingApp), GLFW_KEY_ESCAPE);
 
 }
 
-void configureRendering(){
+void configureRendering() {
 	/*customize Rendermanager, Renderloop, etc. via framelisteners and such*/
 	/* use listener interfaces for: what should happen everytime a frame is drawn */
 
 	/*comment in to use placeholders for Renderloop, rendering every VO of the testingState; change Shader paths to use a different shader*/
 //	testingApp->attachListenerOnProgramInitialization(	new SetDefaultShaderListener( new Shader (SHADERS_PATH "/Phong_Test/phong.vert", SHADERS_PATH "/Phong_Test/phong.frag")));
 //	testingApp->attachListenerOnRenderManagerFrameLoop(	new RenderloopPlaceHolderListener());
-
+	testingApp->attachListenerOnProgramInitialization(
+			new SetDefaultShaderListener(
+					new Shader(SHADERS_PATH "/Phong_Test/phong.vert",
+							SHADERS_PATH "/Phong_Test/phong.frag")));
+	testingApp->attachListenerOnRenderManagerFrameLoop(
+			new RenderloopPlaceHolderListener());
 }
 
-void configureOtherStuff(){
+void configureOtherStuff() {
 	/* customization for other stuff */
 
 }
 
-void configureApplication(){
+void configureApplication() {
 	/* create  minimal Application with one state */
-	testingApp  		= 	Application::getInstance();
-	testingApp 			->	setLabel("PROJEKT PRAKTIKUM");
-	testingState 	= 	new VRState("TESTING FRAMEWORK");
-	testingApp 			->	addState(testingState);
+	testingApp = Application::getInstance();
+	testingApp->setLabel("PROJEKT PRAKTIKUM");
+	testingState = new VRState("TESTING FRAMEWORK");
+	testingApp->addState(testingState);
 	testingInputHandler = testingState->getIOHandler();
 
 	/* configure to satisfaction*/
@@ -79,3 +89,4 @@ int main() {
 	testingApp->run();			// 2 run application
 
 	return 0;				// 3 end :)
+}
