365049742b934e0418aa7c65f475a41abc144597
BatsiMcLovin <kreuzgar@yahoo.de>
2014-03-26 13:18:26 +0100

    constructors for GUIElements

--- src/libraries/GUI/GUIElement.cpp
+++ src/libraries/GUI/GUIElement.cpp
@@ -1,9 +1,26 @@
-/*
- * GUIElement.cpp
- *
- *  Created on: 26.03.2014
- *      Author: Batsi
- */
+#include "GUIElement.h"
+
+
+GUIElement::GUIElement(){
+	position.x = 10;
+	position.x = 10;
+	size.x = 10;
+	size.y = 10;
+
+	}
+
+GUIElement::GUIElement(glm::vec2 position, glm::vec2 psize){
+	this->position = position;
+	this->size = size;
+
+	}
+
+GUIElement::GUIElement(int x, int y, int sizeX, int sizeY){
+	position.x = x;
+	position.y = y;
+	size.x = sizeX;
+	size.y = sizeY;
+	}
 
 
 
