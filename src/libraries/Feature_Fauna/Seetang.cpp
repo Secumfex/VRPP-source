b330cac4b35d74a79fb5c3d2184a63fcabff9722
ddehrmann <ddehrmann@uni-koblenz.de>
2014-03-26 10:44:28 +0100

    fixed

--- src/libraries/Feature_Fauna/Seetang.cpp
+++ src/libraries/Feature_Fauna/Seetang.cpp
@@ -7,10 +7,35 @@
 #include <iostream>
 #include "Seetang.h"
 
+
 using namespace std;
 
+btRigidBody *rigidBody_tmp;
 Seetang::Seetang(){
+
+	vector<btRigidBody*> rbVec;
+	for(btRigidBody i = rbVec.front(); i < rbVec.end(); i++){
+		rbVec[i] = addSphere(10.0, 0.0, 0.0, 0.0, 0.0);
+	}
 
 }
 
+btRigidBody* addSphere(float radius, float x, float y, float z, float mass) {
 
+	btTransform t;
+	t.setIdentity();
+	t.setOrigin(btVector3(x,y,z));
+
+	btSphereShape* sphere = new btSphereShape(radius);
+
+	btVector3 inertia;
+	if(mass != 0.0){
+		sphere->calculateLocalInertia(mass, inertia);
+	}
+
+	btMotionState* motion = new btDefaultMotionState(t);
+	btRigidBody::btRigidBodyConstructionInfo info(mass,motion,sphere,inertia);
+	btRigidBody* body = new btRigidBody(info);
+	body->setLinearFactor(btVector3(1,1,1));
+	return body;
+}
--- src/libraries/Feature_Fauna/Seetang.h
+++ src/libraries/Feature_Fauna/Seetang.h
@@ -7,17 +7,24 @@
 
 #ifndef SEETANG_H_
 #define SEETANG_H_
+#include <iostream>
+#include <vector>
+#include <glm/glm.hpp>
+#include "btBulletDynamicsCommon.h"
+#include "BulletDynamics\Dynamics\btRigidBody.h"
+#include "Physics\PhysicsComponent.h"
+#include "btBulletDynamicsCommon.h"
 
-class Seetang{
+class Seetang {
 
 public:
 	Seetang();
+	btRigidBody* addSphere(float radius, float x, float y, float z, float mass);
+	btRigidBody *rigidBody;
+	vector<btRigidBody*> spheres;
 
 private:
 
-
 };
-
-
 
 #endif /* SEETANG_H_ */
