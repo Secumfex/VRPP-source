8c2fee1430c2d0924b68db5c2787b2b6b90321e2
BatsiMcLovin <kreuzgar@yahoo.de>
2014-03-26 10:51:18 +0100

    Manager added of Overlay

---
+++ src/libraries/GUI/OverlayManager.h
@@ -0,0 +1,15 @@
+/*
+ * OverlayManager.h
+ *
+ *  Created on: 26.03.2014
+ *      Author: Batsi
+ */
+
+#ifndef OVERLAYMANAGER_H_
+#define OVERLAYMANAGER_H_
+
+
+
+
+
+#endif /* OVERLAYMANAGER_H_ */
