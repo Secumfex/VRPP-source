ef497f1e6e3ec4781ed88d089d13e801cb9b6bec
BatsiMcLovin <kreuzgar@yahoo.de>
2014-03-26 10:58:44 +0100

    s

--- src/libraries/GUI/OverlayManager.h
+++ src/libraries/GUI/OverlayManager.h
@@ -1,15 +1,19 @@
-/*
- * OverlayManager.h
- *
- *  Created on: 26.03.2014
- *      Author: Batsi
- */
 
 #ifndef OVERLAYMANAGER_H_
 #define OVERLAYMANAGER_H_
 
+class Overlaymanager : public Singleton<Overlaymanager>, Subject{
+	friend class Singleton<Overlaymanager>;
 
-
-
-
+private:
+	/** \brief constructor
+	 *
+	 */
+	Overlaymanager();
+public:
+	/** \brief destructor
+	 *
+	 */
+    ~Overlaymanager();
+};
 #endif /* OVERLAYMANAGER_H_ */
