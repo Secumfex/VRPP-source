66fc90a5972a6a0fcad38e0d192b0c0136db2731
ddehrmann <ddehrmann@uni-koblenz.de>
2014-04-10 12:10:08 +0200

    chegg dis out

---
+++ src/executables/Feature_Fauna/SeaGrasPhysicsComponent.h
@@ -0,0 +1,25 @@
+/*
+ * SeaGrasPhysicsComponent.h
+ *
+ *  Created on: 10.04.2014
+ *      Author: Dirk Diggler
+ */
+
+#ifndef SEAGRASPHYSICSCOMPONENT_H_
+#define SEAGRASPHYSICSCOMPONENT_H_
+
+#include "PhysicsComponent.h"
+#include "VirtualObjectFactory.h"
+#include <vector>
+
+class SeaGrasPhysicsComponent {
+
+	vector<VirtualObject*> vecCatMull;
+	vector<btGeneric6DofSpringConstraint*> vecConstraints;
+
+public:
+	SeaGrasPhysicsComponent();
+	virtual ~SeaGrasPhysicsComponent();
+};
+
+#endif /* SEAGRASPHYSICSCOMPONENT_H_ */
