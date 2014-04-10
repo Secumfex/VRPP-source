be349b830aa92f24e9c6744822ecf54e76feeead
ddehrmann <ddehrmann@uni-koblenz.de>
2014-04-10 16:50:27 +0200

    Neue Klasse, neue Idee, los gehts!

--- src/executables/Feature_Fauna/Seetang.h
+++ src/executables/Feature_Fauna/Seetang.h
@@ -1,38 +1,23 @@
 #ifndef SEETANG_H_
 #define SEETANG_H_
-#include <iostream>
-#include <vector>
-#include <glm/glm.hpp>
-#include "btBulletDynamicsCommon.h"
-#include "BulletDynamics\Dynamics\btRigidBody.h"
-#include "Physics\PhysicsComponent.h"
-#include "btBulletDynamicsCommon.h"
-#include "../libraries/Feature_Fauna/Seetang.h"
-#include "BulletDynamics/Dynamics/btRigidBody.h"
-#include "IO/IOManager.h"
-#include "IO/PlayerCamera.h"
-#include "Physics/UpdatePhysicsComponentListener.h"
-#include "Physics/PhysicWorld.h"
-#include "Physics/PhysicWorldSimulationListener.h"
-#include "BulletDynamics/ConstraintSolver/btPoint2PointConstraint.h"
-#include "BulletDynamics/ConstraintSolver/btGeneric6DofConstraint.h"
+
+#include <Visuals/VirtualObject.h>
+#include <list>
 
 
 
 
 class Seetang {
+
 private:
 
 
+
 public:
-	vector<VirtualObject*> mVOVec;
-	float x;
-	float y;
-	float z;
+
 
 	Seetang();
-   // VirtualObject SetUpGroundTarget();
-    VirtualObject SetUpFurtherTarget();
+	VirtualObject SetUpGroundTarget();
 
 };
 
