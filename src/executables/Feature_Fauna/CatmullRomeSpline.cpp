4d7646719c4164c705766b06f4d711e255965a46
TreCool90 <oliverplauk@uni-koblenz.de>
2014-04-22 09:28:40 +0200

    Programm läuft langsam wieder.
    Bis auf die Main ist nur alles auskommentiert.

--- src/executables/Feature_Fauna/CatmullRomeSpline.cpp
+++ src/executables/Feature_Fauna/CatmullRomeSpline.cpp
@@ -1,33 +1,33 @@
-#include "CatmullRomeSpline.h"
-#include <iostream>
-using namespace std;
-
-
-CatmullRomeSpline::CatmullRomeSpline(){
-
-	float Tk = 0.5;				 				//Krümmungsvariable der Kurve
-	float Ab = 0.1;								//Abstandsvariable im Spline
-	int RV0 = 0;								//Richtungsvektoren zum Vertexziehen
-	int RV1 = 1;
-}
-
-CatmullRomeSpline::~CatmullRomeSpline(){}
-
-glm::mat4 CatmullRomeSpline::getViewMatrix(){
-	//mViewMatrix = Camera::getViewMatrix();
-	return mViewMatrix;
-}
-
-void CatmullRomeSpline::MVMultiplikation(){
-	//btRigidBody *= mViewMatrix;
-	//cout<<btRigidBody;
-}
-
-//void CatmullRomeSpline::Spline(VirtualObject* Sphere1,VirtualObject* Sphere2,VirtualObject* Sphere3,VirtualObject* Sphere4){
-
-
+//#include "CatmullRomeSpline.h"
+//#include <iostream>
+//using namespace std;
+//
+//
+//CatmullRomeSpline::CatmullRomeSpline(){
+//
+//	float Tk = 0.5;				 				//Krümmungsvariable der Kurve
+//	float Ab = 0.1;								//Abstandsvariable im Spline
+//	int RV0 = 0;								//Richtungsvektoren zum Vertexziehen
+//	int RV1 = 1;
 //}
-
-void CatmullRomeSpline::Value(){
-
-}
+//
+//CatmullRomeSpline::~CatmullRomeSpline(){}
+//
+//glm::mat4 CatmullRomeSpline::getViewMatrix(){
+//	//mViewMatrix = Camera::getViewMatrix();
+//	return mViewMatrix;
+//}
+//
+//void CatmullRomeSpline::MVMultiplikation(){
+//	//btRigidBody *= mViewMatrix;
+//	//cout<<btRigidBody;
+//}
+//
+////void CatmullRomeSpline::Spline(VirtualObject* Sphere1,VirtualObject* Sphere2,VirtualObject* Sphere3,VirtualObject* Sphere4){
+//
+//
+////}
+//
+//void CatmullRomeSpline::Value(){
+//
+//}
--- src/executables/Feature_Fauna/Seetang.h
+++ src/executables/Feature_Fauna/Seetang.h
@@ -1,24 +1,24 @@
-#ifndef SEETANG_H_
-#define SEETANG_H_
-
-#include <Visuals/VirtualObject.h>
-#include <list>
-
-
-
-
-class Seetang {
-
-private:
-
-
-
-public:
-
-
-	Seetang();
-	VirtualObject SetUpGroundTarget();
-
-};
-
-#endif /* SEETANG_H_ */
+//#ifndef SEETANG_H_
+//#define SEETANG_H_
+//
+//#include <Visuals/VirtualObject.h>
+//#include <list>
+//
+//
+//
+//
+//class Seetang {
+//
+//private:
+//
+//
+//
+//public:
+//
+//
+//	Seetang();
+//	VirtualObject SetUpGroundTarget();
+//
+//};
+//
+//#endif /* SEETANG_H_ */
