13ea3144185aa53d92c31f05c1dbeb99a2ccfd1a
Batsi <kreuzgar@yahoo.de>
2014-09-30 09:26:26 +0200

    TestClass

---
+++ src/executables/GBuffer/ShaderProgram.cpp
@@ -0,0 +1,18 @@
+/*
+ * ShaderProgram.cpp
+ *
+ *  Created on: 30.09.2014
+ *      Author: Batsi
+ */
+
+#include "C:ShaderProgram.h"
+
+ShaderProgram::ShaderProgram() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ShaderProgram::~ShaderProgram() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/executables/GBuffer/ShaderProgram.h
@@ -0,0 +1,17 @@
+/*
+ * ShaderProgram.h
+ *
+ *  Created on: 30.09.2014
+ *      Author: Batsi
+ */
+
+#ifndef SHADERPROGRAM_H_
+#define SHADERPROGRAM_H_
+
+class ShaderProgram {
+public:
+	ShaderProgram();
+	virtual ~ShaderProgram();
+};
+
+#endif /* SHADERPROGRAM_H_ */
